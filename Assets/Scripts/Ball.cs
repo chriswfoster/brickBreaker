babc751f62c393465e03b741f91251254ea7bd1f
chriswfoster <chriswfoster@gmail.com>
2019-10-09 22:50:39 -0500

    Added autoplay and wrapped game up

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 
 	public Paddle paddle; // grant access of paddle to ball
-	public bool hastStarted = false;
+	public bool hasStarted = false;
 	private Vector3 paddleToBallVector;
 	
 	// Use this for initialization
@@ -12,16 +12,26 @@ public class Ball : MonoBehaviour {
 		paddle = GameObject.FindObjectOfType<Paddle>();
 		paddleToBallVector = this.transform.position - paddle.transform.position;
 		print (paddleToBallVector);
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision){
+		Vector2 tweak = new Vector2(Random.Range (-0.2f, 0.2f), Random.Range(0f, 0.2f));
+		
+		if(hasStarted){
+			audio.Play();
+			rigidbody2D.velocity += tweak; // lowercase r because it's an instance of the rigidbody
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!hastStarted) {
+		if(!hasStarted) {
 			this.transform.position = paddle.transform.position + paddleToBallVector; // locks ball to pedal, waiting for mouse press for launch
 			if (Input.GetMouseButtonDown(0)){ // reminder: control and apostrophe to get the info on this method
-				hastStarted = true;
+				hasStarted = true;
 				print("Button clicked");
-				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+				this.rigidbody2D.velocity = new Vector2(Random.Range (-2.0f, 2.0f), 10f);
 			}
 		}
 		
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,15 +2,51 @@
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
-
+	private Ball ball;
 	float mousePosinBlocks;
+	public bool autoPlay = false;
 	// Use this for initialization
 	void Start () {
-	
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!autoPlay){
+			MoveWithMouse ();
+		} else {
+			AutoPlay ();
+		}
+	}
+	
+	void AutoPlay(){
+		if (ball.hasStarted){
+			Vector3 paddlePos = new Vector3(Mathf.Clamp(mousePosinBlocks, 0.5f, 15.5f), this.transform.position.y, 0f);// the f signifies float
+			Vector3 ballPos = ball.transform.position;
+			paddlePos.x = Mathf.Clamp (ballPos.x, 0.5f, 15.5f);
+			this.transform.position = paddlePos;
+		
+		} else {
+			//print (Input.mousePosition.x); // if you type the x, you'll see a red F for field
+			//print(Input.mousePosition.x / Screen.width); // converts the whole x position to a basic 0-1 scale, no matter how big the width is.
+			//print (Input.mousePosition.x / Screen.width * 16); // multiply it by 16, since that's how many game units wide we are, it'll give us that number. 
+			
+			mousePosinBlocks = Input.mousePosition.x / Screen.width * 16;
+			Vector3 paddlePos = new Vector3(Mathf.Clamp(mousePosinBlocks, 0.5f, 15.5f), this.transform.position.y, 0f);// the f signifies float
+			//this.transform.position = Vector3(mousePosinBlocks, 0.00, 0.00); // have to instantiate a NEW one
+			
+			// you can access paddlePos x, y, z, with paddlePos.x (for example)
+			
+			this.transform.position = paddlePos; // this keywoard is the instance of the component script. It is the paddle Script itself.
+			// could probably use transform.position without this, and it should still work.
+		}
+
+
+	}
+	
+	void MoveWithMouse(){ // for automated testing!
+
 		//print (Input.mousePosition.x); // if you type the x, you'll see a red F for field
 		//print(Input.mousePosition.x / Screen.width); // converts the whole x position to a basic 0-1 scale, no matter how big the width is.
 		//print (Input.mousePosition.x / Screen.width * 16); // multiply it by 16, since that's how many game units wide we are, it'll give us that number. 
@@ -22,6 +58,6 @@ public class Paddle : MonoBehaviour {
 		// you can access paddlePos x, y, z, with paddlePos.x (for example)
 		
 		this.transform.position = paddlePos; // this keywoard is the instance of the component script. It is the paddle Script itself.
-												// could probably use transform.position without this, and it should still work.
+		// could probably use transform.position without this, and it should still work.
 	}
 }
